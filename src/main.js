cfe0421fd1e1ae6bf126d1d541ede86afdb8d633
tatosf <santiagofischel1@gmail.com>
2023-12-08 20:47:02 +0100

    try 1 logs to appinsights

--- src/main.js
+++ src/main.js
@@ -5,13 +5,24 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
 
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue);
 
+// Initialize Application Insights
+const appInsights = new ApplicationInsights({
+    config: {
+        instrumentationKey: process.env.VUE_APP_APPINSIGHTS_KEY,
+        
+    }
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Tracks page views
+
 new Vue({
   router,
-  store, // Add the store to your Vue instance
+  store,
   render: h => h(App)
 }).$mount('#app');
